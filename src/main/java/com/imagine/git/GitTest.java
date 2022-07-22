d3acb2f45d3461ca2cd7a4b38e640b1d35cf12da
Imagine <zyh10_27@petalmail.com>
2022-07-22 18:08:45 +0800

    gitee commit

--- src/main/java/com/imagine/git/GitTest.java
+++ src/main/java/com/imagine/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("HelloWorld4");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("gitee test");
     }
 }
