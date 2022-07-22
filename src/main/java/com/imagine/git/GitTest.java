e1250a26c18980cb7bee9e35d40addd90a705774
imaginekk <109642579+imaginekk@users.noreply.github.com>
2022-07-22 17:29:26 +0800

    Update GitTest.java

--- src/main/java/com/imagine/git/GitTest.java
+++ src/main/java/com/imagine/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("HelloWorld5");
         System.out.println("HelloWorld4");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
