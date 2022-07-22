9b8b540516e9befb216b19088042607e2f71da3e
Imagine <zyh10_27@petalmail.com>
2022-07-22 15:04:23 +0800

    hot-fix first commit

--- src/main/java/com/imagine/git/GitTest.java
+++ src/main/java/com/imagine/git/GitTest.java
@@ -2,12 +2,13 @@ package com.imagine.git;
 
 /**
  * @author imagine
- * @date 2022/7/22 - 11:38
+ *
  */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
         System.out.println("HelloWorld2");
         System.out.println("HelloWorld3");
+        System.out.println("HelloWorld4");
     }
 }
