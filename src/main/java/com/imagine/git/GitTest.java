e51ed2d35447b7cfc5838271aebe3f903ec0e843
Imagine <zyh10_27@petalmail.com>
2022-07-22 11:50:32 +0800

    first commit

---
+++ src/main/java/com/imagine/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.imagine.git;
+
+/**
+ * @author imagine
+ * @date 2022/7/22 - 11:38
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
